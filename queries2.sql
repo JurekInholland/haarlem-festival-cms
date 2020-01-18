f750ad97100814e26b91c869bc95720aa2bbfce4
JurekInholland <jurekbaumann@me.com>
2020-01-18 21:08:10 +0100

    Add query for static_pages default values

--- queries2.sql
+++ queries2.sql
@@ -36,3 +36,12 @@ VALUES
 	(1, 'Restaurant Mr. & Mrs.', 'Lange Veerstraat 4, 2011 DB Haarlem, Nederland', NULL, NULL),
 	(2, 'Ratatouille', 'Spaarne 96, 2011 CL Haarlem, Nederland', NULL, NULL),
 	(3, 'Restaurant ML', 'Kleine Houtstraat 70, 2011 DR Haarlem, Nederland', NULL, NULL);
+
+
+INSERT INTO `static_pages` (`id`, `slug`, `headline`, `content`, `edited`, `menu`, `image`)
+VALUES
+	(28, 'links', 'Userful Links', 'here are some useful links\r\n', '2020-01-18 05:23:49', 1, NULL),
+	(1, 'test', 'Test', 'This is a <u>test page</u>', '2020-01-17 05:07:24', 0, NULL),
+	(20, 'parking', 'Parking', 'You can find detailed <b>parking</b> information here.', '2020-01-17 09:53:13', 1, NULL),
+	(24, 'faq', 'FAQ', 'Frequently asked questions:\r\n<h1>Q: What is the meaning of life? </h1>\r\n<h3>A: Programming PHP apps. </h3>', '2020-01-17 09:56:11', 1, NULL),
+	(27, 'about', 'About', 'about page', '2020-01-18 05:22:49', 1, NULL);
