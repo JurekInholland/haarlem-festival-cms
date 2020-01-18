e1924a776b8c31e4e8134b070f64622c0fffbc83
Jurek Baumann <jurekbaumann@me.com>
2020-01-18 21:38:59 +0100

    Clean up queries

--- queries.sql
+++ queries.sql
@@ -1,48 +1,24 @@
-DROP TABLE IF EXISTS new_events, festival_events, tickets, static_pages, event_categories
-event_locations, cms_users, festival_info;
+DROP TABLE IF EXISTS new_events, festival_events, tickets, static_pages, event_categories,
+event_locations, cms_users, cms_login_tokens, festival_info, cms_customer_data, invoices, restaurants;
 
 
--- Festival Setup
-
--- NEW EVENTS
-CREATE TABLE `new_events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `description` text NULL,
-  `start_date` datetime NOT NULL,
-  `end_date` datetime NOT NULL,
-  `location` varchar(200) NOT NULL DEFAULT '',
-  `category` tinyint(4) NOT NULL DEFAULT '0',
-  `price` double DEFAULT 0,
-  `artist` varchar(100) NOT NULL,
-  `subject` int(11) NULL,
-  `slug` varchar(100) NOT NULL,
-  
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `slug` (`slug`)
-
-) ENGINE=InnoDB;
-
-
--- final events table
 CREATE TABLE `festival_events` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`type` INT(11) NOT NULL,
-	`start_date` DATETIME NULL DEFAULT NULL,
-	`end_date` DATETIME NULL DEFAULT NULL,
-	`address` VARCHAR(100) NULL DEFAULT NULL,
-	`location_detail` VARCHAR(100) NULL DEFAULT NULL,
-	`name` VARCHAR(100) NOT NULL,
-	`tickets` INT(11) NULL DEFAULT NULL,
-	`price` DOUBLE NULL DEFAULT NULL,
-	`description` TEXT NULL,
-	`rating` INT(11) NULL DEFAULT NULL,
-	`image` CHAR(16) NULL DEFAULT NULL,
-	`slug` VARCHAR(100) DEFAULT NULL,
-	PRIMARY KEY (`id`)
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `type` INT(11) NOT NULL,
+    `start_date` DATETIME NULL DEFAULT NULL,
+    `end_date` DATETIME NULL DEFAULT NULL,
+    `address` VARCHAR(100) NULL DEFAULT NULL,
+    `location_detail` VARCHAR(100) NULL DEFAULT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `tickets` INT(11) NULL DEFAULT NULL,
+    `price` DOUBLE NULL DEFAULT NULL,
+    `description` TEXT NULL,
+    `rating` INT(11) NULL DEFAULT NULL,
+    `image` CHAR(16) NULL DEFAULT NULL,
+    `slug` VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-INSERT INTO festival_events (id, type, start_date, end_date, address, location_detail, name, tickets, price, description, rating, image)
-VALUES (1, 0, )
 
 -- Tickets
 CREATE TABLE `tickets` (
@@ -54,6 +30,7 @@ CREATE TABLE `tickets` (
   `TICKET_SCANNED` tinyint(4) DEFAULT '0',
   `order_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `paid_date` datetime DEFAULT NULL,
+  `invoice_id` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`ticket_id`)
 ) ENGINE=InnoDB;
 
@@ -74,18 +51,18 @@ CREATE TABLE `static_pages` (
 CREATE TABLE `event_categories` (
 	`id` INT(11) NOT NULL,
 	`category` VARCHAR(50) NOT NULL,
+    `color` CHAR(6) NOT NULL DEFAULT '',
+    `slug` VARCHAR(50) DEFAULT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-INSERT INTO `event_categories` (`id`, `category`) VALUES (0, 'Haarlem Jazz');
-INSERT INTO `event_categories` (`id`, `category`) VALUES (1, 'Haarlem Dance');
-INSERT INTO `event_categories` (`id`, `category`) VALUES (2, 'Haarlem Food');
-INSERT INTO `event_categories` (`id`, `category`) VALUES (3, 'Haarlem Historic');
 
 -- Event locations
 CREATE TABLE `event_locations` (
 	`id` INT(11) NOT NULL,
-	`location` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL DEFAULT '',
+    `address` VARCHAR(100) DEFAULT '',
+    `category` INT(11) NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
@@ -93,22 +70,7 @@ CREATE TABLE `event_locations` (
 CREATE TABLE `festival_info` (
 	`start_date` DATETIME NULL DEFAULT NULL,
 	`end_date` DATETIME NULL DEFAULT NULL
-)
-ENGINE=InnoDB
-;
-
-CREATE TABLE `events` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(100) NOT NULL,
-	`description` TEXT NOT NULL,
-	`start_time` DATETIME NOT NULL,
-	`end_time` DATETIME NOT NULL,
-	`location` VARCHAR(200) NOT NULL,
-	`category` TINYINT(4) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-;
+) ENGINE=InnoDB;
 
 
 CREATE TABLE `cms_users` (
@@ -119,11 +81,47 @@ CREATE TABLE `cms_users` (
 	`role` TINYINT(4) NOT NULL DEFAULT '0',
 	`registration_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-;
+) ENGINE=InnoDB;
+
+CREATE TABLE `cms_login_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` char(64) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+--   UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `cms_customer_data` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) NOT NULL DEFAULT '',
+  `lastname` varchar(50) DEFAULT NULL,
+  `customer_address` varchar(100) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `invoices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `invoice_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `restaurants` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `address` varchar(200) NOT NULL DEFAULT '',
+  `food_type` text,
+  `weekdays` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
 
+INSERT INTO `event_categories` (`id`, `category`, `color`, `slug`)
+VALUES
+    (0, 'Haarlem Jazz', '169BD5', 'jazz'),
+    (1, 'Haarlem Dance', 'C32039', 'dance'),
+    (2, 'Haarlem Food', 'F59F17', 'food');
 
 -- Debug locations
 INSERT INTO `event_locations` (`id`, `name`, `address`, `category`)
@@ -135,7 +133,6 @@ VALUES
 	(5, 'Restaurant ML', 'Kleine Houtstraat 70, 2011 DR Haarlem, Nederland', 1),
 	(6, 'Club Stalker', 'Kromme Elleboogsteeg 20, 2011 TS Haarlem', 2),
 	(7, 'Caprera Openluchttheater ', 'Hoge Duin en Daalseweg 2, 2061 AG Bloemendaal', 2);
-
 
 
 INSERT INTO `festival_events` (`id`, `type`, `start_date`, `end_date`, `address`, `location_detail`, `name`, `tickets`, `price`, `description`, `rating`, `image`, `slug`)
@@ -153,9 +150,39 @@ VALUES
 	(12, 0, '2020-08-28 19:30:00', '2020-08-28 20:30:00', 'Patronaat', 'Second', 'Ruis SoundSystem', 200, 10, 'Consist of five members that plays jazz and blues with a mix of modern pop culture music.', NULL, NULL, 'ruis-soundsystem'),
 	(15, 0, '2020-08-28 21:00:00', '2020-08-28 22:00:00', 'Patronaat', 'Second', 'The Family XL', 200, 10, 'In 2015, Xander Hubrecht decided that he wanted to be on stage again with his own work. He started a project called \"The Family XL\" and performed with it every mont in the Jopenkerk in Haarlem.', NULL, NULL, 'the-family-xl'),
 	(16, 0, '2020-08-29 18:00:00', '2020-08-29 19:00:00', 'Patronaat', 'Main', 'Gare Du Nord', 300, 15, 'Gare du Nord is a Dutch-Belgian jazz band, originally consisting of Doc (Ferdi Lancee) and Inca (Barend Fransen). Doc played guitar and Inca played saxophone.', NULL, NULL, 'gare-du-nord'),
-	(17, 0, '2020-08-29 19:30:00', '2020-08-29 20:30:00', 'Patronaat', 'Main', 'Rilan & The Bombadiers', 300, 15, 'With a sold out first clubtour, a booming festival season and tracks that have already been featured in a number of big Hollywood productions, (Netflix / HULU / FOX: Shooter, Shut Eye and Rosewood) this band has certainly been keeping busy.', NULL, NULL, NULL),
+	(17, 0, '2020-08-29 19:30:00', '2020-08-29 20:30:00', 'Patronaat', 'Main', 'Rilan & The Bombadiers', 300, 15, 'With a sold out first clubtour, a booming festival season and tracks that have already been featured in a number of big Hollywood productions, (Netflix / HULU / FOX: Shooter, Shut Eye and Rosewood) this band has certainly been keeping busy.', NULL, NULL, 'rilan-and-the-bombadiers'),
 	(18, 0, '2020-08-29 21:00:00', '2020-08-29 22:00:00', 'Patronaat', 'Main', 'Soul Six', 300, 15, 'Soul Six is (Moshe Tamir) from Tel Aviv Israel . One of the mainstays industry of electronic music in Israel & Responsible for many successful events in our country.', NULL, NULL, 'soul-six'),
 	(19, 0, '2020-08-29 18:00:00', '2020-08-29 19:00:00', 'Patronaat', 'Third', 'Han Bennink', 150, 10, 'Drummer and multi-instrumentalist Han Bennink was born in Zaandam near Amsterdam in 1942. His first percussion instrument was a kitchen chair.', NULL, NULL, 'han-bennink'),
 	(20, 0, '2020-08-29 19:30:00', '2020-08-29 20:30:00', 'Patronaat', 'Third', 'The Nordanians', 150, 10, 'When Oene van Geel viola, Mark Tuinstra guitar and Niti Ranjan Biswas tabla virtuoso played together for the first time there where immediately fireworks.', NULL, NULL, 'the-nordanians'),
 	(21, 0, '2020-08-29 21:00:00', '2020-08-29 22:00:00', 'Patronaat', 'Third', 'Lilith Merlot', 150, 10, 'With its first release in two years, the single \"Prepping Men,\" Haarlem-based Lilith Merlot has introduced the audience to her new sound.', NULL, NULL, 'lilith-merlot'),
 	(22, 0, '2020-08-30 15:00:00', '2020-08-30 16:00:00', 'Grote Markt', NULL, 'Ruis Soundsystem', NULL, 0, 'Consist of five members that plays jazz and blues with a mix of modern pop culture music..', NULL, NULL, 'ruis-soundsystem');
+
+
+INSERT INTO `restaurants` (`id`, `name`, `address`, `food_type`, `weekdays`)
+VALUES
+	(1, 'Restaurant Mr. & Mrs.', 'Lange Veerstraat 4, 2011 DB Haarlem, Nederland', NULL, NULL),
+	(2, 'Ratatouille', 'Spaarne 96, 2011 CL Haarlem, Nederland', NULL, NULL),
+	(3, 'Restaurant ML', 'Kleine Houtstraat 70, 2011 DR Haarlem, Nederland', NULL, NULL);
+
+
+INSERT INTO `static_pages` (`id`, `slug`, `headline`, `content`, `edited`, `menu`, `image`)
+VALUES
+	(28, 'links', 'Userful Links', 'here are some useful links\r\n', '2020-01-18 05:23:49', 1, NULL),
+	(1, 'test', 'Test', 'This is a <u>test page</u>', '2020-01-17 05:07:24', 0, NULL),
+	(20, 'parking', 'Parking', 'You can find detailed <b>parking</b> information here.', '2020-01-17 09:53:13', 1, NULL),
+	(24, 'faq', 'FAQ', 'Frequently asked questions:\r\n<h1>Q: What is the meaning of life? </h1>\r\n<h3>A: Programming PHP apps. </h3>', '2020-01-17 09:56:11', 1, NULL),
+	(27, 'about', 'About', 'about page', '2020-01-18 05:22:49', 1, NULL);
+
+INSERT INTO `cms_users` (`id`, `username`, `email`, `password`, `role`, `registration_date`) VALUES (1, 'jurek', 'jurekmail', '$2y$10$ygG9cZdRUjrFOnYa2X7iIujA4fkeaXCdEPkrAeH4zTl/Garkn2Xie', 1, '2020-01-11 23:34:35');
+INSERT INTO `cms_users` (`id`, `username`, `email`, `password`, `role`, `registration_date`) VALUES (2, 'admin', 'admin@mail.com', '$2y$10$jTf0H2Q9TRCPB6SSpVtpMOektMmhoesc6RixEk/c4knr/2N4jXZam', 1, '2020-01-18 19:38:59');
+INSERT INTO `cms_users` (`id`, `username`, `email`, `password`, `role`, `registration_date`) VALUES (3, 'superadmin', 'superadmin@mail.com', '$2y$10$0I/u/vzyURrucGwGcmp7DOrgxNSH7cqPp3i6cJ6uGwfp17554pVZu', 2, '2020-01-18 19:39:00');
+INSERT INTO `cms_users` (`id`, `username`, `email`, `password`, `role`, `registration_date`) VALUES (4, 'user', 'user@mail.com', '$2y$10$OO3f7uRoIfQ77mFqsQvAKOsIKeDQhCSjYF6lfkRzsMLqz4RZPQNU.', 0, '2020-01-18 20:09:21');
+
+INSERT INTO `cms_customer_data` (`user_id`, `firstname`, `lastname`, `customer_address`, `phone`) VALUES (1, 'Jurek', 'Baumann', 'Donkere Spaarne 44, 2e', '123123123');
+
+INSERT INTO `festival_info` (`start_date`, `end_date`) VALUES ('2020-06-26 08:00:00', '2020-06-29 22:00:00');
+
+INSERT INTO `invoices` (`id`, `user_id`, `invoice_date`) VALUES (123, 1, '2020-01-18 19:49:53');
+
+INSERT INTO `tickets` (`ticket_id`, `user_id`, `event_id`, `amount`, `IS_PAID`, `TICKET_SCANNED`, `order_date`, `paid_date`, `invoice_id`) VALUES ('aP8aDAjivJxCanbdLG', 1, 18, 2, 0, 0, '2020-01-18 20:54:38', NULL, 123);
+INSERT INTO `tickets` (`ticket_id`, `user_id`, `event_id`, `amount`, `IS_PAID`, `TICKET_SCANNED`, `order_date`, `paid_date`, `invoice_id`) VALUES ('yAvcCkwSEYO8YyLw2R', 1, 10, 1, 0, 0, '2020-01-18 20:54:32', NULL, 123);
